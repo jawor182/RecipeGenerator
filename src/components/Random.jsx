fb5587ba85639a7aea7a56b2bba79063b9dbf0c6
jawor182 <kubajaworski59@outlook.com>
2023-08-03 15:35:22 +0200

    added filters to random module

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -5,10 +5,13 @@ const Random = () =>{
     const [title,setTitle] = useState("");
     const [image,setImage] = useState("");
     const [url,setUrl] = useState("");
+    const [type,setType] = useState("");
 
 
 const apiKey = "d8231659ec8b4d1a9547c1dde34e9da9";
-
+  const handleTypeChange = (event) => {
+    setType(event.target.value);
+  };
   const handleSubmit = async (event) => {
     const results = document.querySelector('.results');
     const container = document.querySelector('.container');
@@ -16,7 +19,7 @@ const apiKey = "d8231659ec8b4d1a9547c1dde34e9da9";
     event.preventDefault();
     const options = {
         method:"GET",
-        url: new URL(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`)
+        url: new URL(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&tags=${type}`),
     }
     try{
         const response = await axios.request(options);
@@ -34,6 +37,18 @@ const apiKey = "d8231659ec8b4d1a9547c1dde34e9da9";
   <>
   <div>
     <form onSubmit={handleSubmit} className="form2">
+      <label className="label">Select Type:</label>
+      <select className="select" value={type} onChange={handleTypeChange}>
+        <option value="dinner">Dinner</option>
+        <option value="breakfast">Breakfast</option>
+        <option value="main course">Main course</option>
+        <option value="salad">Salad</option>
+        <option value="snack">Snack</option>
+        <option value="dessert">Dessert</option>
+        <option value="side dish">Side dish</option>
+        <option value="beverage">Beverage</option>
+        <option value="drink">Drink</option>
+      </select>
         <label className="label">Click and get random dish!</label>
         <input type="submit" className="randomBtn"  value="Click me!"></input>
     </form>
